7f94f4d653ea59798dedb565666633455d687ed2
Craig Buchanan <craig@hoarde.net>
2026-05-03 14:31:01 +0100

    fixing behaviour when there's no mode selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,12 @@ The visualiser will skip Wednesday unless there are events, and will skip hours 
 # options are; hardcoded, textbox
 mode = st.menu_button(label="Mode: Test or Live", options=["hardcoded", "textbox"], help="Selects Live or Test modes; Mostly here so that I don't break things while experimenting")
 
-# get the two pages of wishlist
-if mode == "hardcoded":
+# get the two pages of wishlist if at all possible
+if mode == None:
+    # there has been no selection yet, so set wishlist pages to None
+    wishlistPageOne = None
+    wishlistPageTwo = None
+elif mode == "hardcoded":
     # populate from the hardcoded data
     wishlistPageOne = rawWishlist.pageOne
     wishlistPageTwo = rawWishlist.pageTwo
@@ -28,31 +32,33 @@ elif mode == "textbox":
     wishlistPageOne = st.text_area(label="Wishlist Page One", help="Copy-Paste into here from page 1 of your wishlist, priorties 1-25; If you select from the middle of the word 'prority' & down to the end of the time of the last event on the list, that should do the job")
     wishlistPageTwo = st.text_area(label="Wishlist Page Two", help="Copy-Paste into here from page 2 of your wishlist, priorties 26-50; If you select from the middle of the word 'prority' & down to the end of the time of the last event on the list, that should do the job")
 
-## if there's nothing in the text boxes, don't draw the table, just display a message
-if (wishlistPageOne is None or len(wishlistPageOne)==0) and (wishlistPageTwo is None or len(wishlistPageTwo)==0):
-    # the text boxes are empty
-    st.header("Wishlist Visualisation Will Only Be Created When There Is Wishlist Data", divider=True)
-else:
-    # add a header
-    st.header("Wishlist Visualisation", divider=True)
-    
-    # build the list of event lists by parsing each wishlist page
-    pageOneListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageOne)
-    pageTwoListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageTwo)
-    # combine the lists into one
-    listOfEventLists = pageOneListOfEventLists + pageTwoListOfEventLists
+## there may not yet be a mode selection, so only do something if mode has a value
+if mode is not None:
+    ## if there's nothing in the text boxes, don't draw the table, just display a message
+    if (wishlistPageOne is None or len(wishlistPageOne)==0) and (wishlistPageTwo is None or len(wishlistPageTwo)==0):
+        # the text boxes are empty
+        st.header("Wishlist Visualisation Will Only Be Created When There Is Wishlist Data", divider=True)
+    else:
+        # add a header
+        st.header("Wishlist Visualisation", divider=True)
+        
+        # build the list of event lists by parsing each wishlist page
+        pageOneListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageOne)
+        pageTwoListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageTwo)
+        # combine the lists into one
+        listOfEventLists = pageOneListOfEventLists + pageTwoListOfEventLists
 
-    # get a list of event dicts
-    listOfEventDicts = EventListParsing.listOfEventListsToListOfEventDicts(listOfEventLists)
+        # get a list of event dicts
+        listOfEventDicts = EventListParsing.listOfEventListsToListOfEventDicts(listOfEventLists)
 
-    # add event hours to the event dicts
-    listOfEventDicts = EventListParsing.addEventHours(listOfEventDicts)
+        # add event hours to the event dicts
+        listOfEventDicts = EventListParsing.addEventHours(listOfEventDicts)
 
-    # create a dict of lists, one for each hour where there's an event
-    visualiserDict = EventListParsing.createWishlistVisualiserDict(listOfEventDicts)
+        # create a dict of lists, one for each hour where there's an event
+        visualiserDict = EventListParsing.createWishlistVisualiserDict(listOfEventDicts)
 
-    # create an HTML table from the visualiser dict
-    visualiserHTML = EventListParsing.createHTMLVisualisation(visualiserDict)
+        # create an HTML table from the visualiser dict
+        visualiserHTML = EventListParsing.createHTMLVisualisation(visualiserDict)
 
-    # display the table
-    st.html(visualiserHTML)
+        # display the table
+        st.html(visualiserHTML)
