ca93f741050743c84b1db456692858510436f9df
Craig Buchanan <craig@hoarde.net>
2026-05-03 20:33:50 +0100

    removing the test mode switch

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ Contains the orchestration for the Gen Con Event Wishlist Visualiser in Streamli
 """
 
 import streamlit as st
-import rawWishlist
 import EventListParsing
 
 # set up a title & some explanation
@@ -12,55 +11,40 @@ st.text("""Creates a visualisation of a Gen Con Event Wishlist, so that you can 
         
 The visualiser will skip Wednesday unless there are events, and will skip hours before 8am unless there are events.""")
 
-## switch to go between different modes
-mode = st.menu_button(label="Mode: Test or Live", options=["Test (hardcoded)", "Live (textbox)"], help="Selects Live or Test modes; Mostly here so that I don't break things while experimenting")
-# set to Live if not already set, so that the app does something
-if mode is None:
-    mode = "Live (textbox)"
+# add a label for the text box section
+st.header("Wishlist Information Goes Here", divider=True)
+# add some instructions
+st.text("Copy-Paste from your wishlist page into the text areas below, priorities 1-25 in the first area & 26-50 in the second; If you just hit Ctrl+A to select the whole webpage, Ctrl+C to copy the whole webpage, and Ctrl+P to paste it, that should do the job")
 
-# get the two pages of wishlist if at all possible
-if mode == "Test (hardcoded)":
-    # populate from the hardcoded data
-    wishlistPageOne = rawWishlist.pageOne
-    wishlistPageTwo = rawWishlist.pageTwo
-    # tell the user about it
-    st.text("Populating using a wishlist from 2024, to show how it works")
-elif mode == "Live (textbox)":
-    # add a label for the text box section
-    st.header("Wishlist Information Goes Here", divider=True)
-    # add some instructions
-    st.text("Copy-Paste from your wishlist into the text areas below, priorties 1-25 in the first area & 26-50 in the second; If you select from the middle of the word 'prority' & down to the end of the time of the last event on the list, that should do the job")
-    # take from the text boxes
-    wishlistPageOne = st.text_area(label="Wishlist Page One", help="Copy-Paste into here from page 1 of your wishlist, priorties 1-25; If you select from the middle of the word 'prority' & down to the end of the time of the last event on the list, that should do the job")
-    wishlistPageTwo = st.text_area(label="Wishlist Page Two", help="Copy-Paste into here from page 2 of your wishlist, priorties 26-50; If you select from the middle of the word 'prority' & down to the end of the time of the last event on the list, that should do the job")
+# take from the text boxes
+wishlistPageOne = st.text_area(label="Wishlist Page One", help="Copy-Paste page 1 of your wishlist, priorties 1-25, into here")
+wishlistPageTwo = st.text_area(label="Wishlist Page Two", help="Copy-Paste page 2 of your wishlist, priorties 26-50, into here")
 
-## there may not yet be a mode selection, so only do something if mode has a value
-if mode is not None:
-    ## if there's nothing in the text boxes, don't draw the table, just display a message
-    if (wishlistPageOne is None or len(wishlistPageOne)==0) and (wishlistPageTwo is None or len(wishlistPageTwo)==0):
-        # the text boxes are empty
-        st.header("Wishlist Visualisation Will Only Be Created When There Is Wishlist Data", divider=True)
-    else:
-        # add a header
-        st.header("Wishlist Visualisation", divider=True)
-        
-        # build the list of event lists by parsing each wishlist page
-        pageOneListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageOne)
-        pageTwoListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageTwo)
-        # combine the lists into one
-        listOfEventLists = pageOneListOfEventLists + pageTwoListOfEventLists
+## if there's nothing in the text boxes, don't draw the table, just display a message
+if (wishlistPageOne is None or len(wishlistPageOne)==0) and (wishlistPageTwo is None or len(wishlistPageTwo)==0):
+    # the text boxes are empty
+    st.header("Wishlist Visualisation Will Only Be Created When There Is Wishlist Data", divider=True)
+else:
+    # add a header
+    st.header("Wishlist Visualisation", divider=True)
+    
+    # build the list of event lists by parsing each wishlist page
+    pageOneListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageOne)
+    pageTwoListOfEventLists = EventListParsing.wishlistTextToListOfEventLists(wishlistPageTwo)
+    # combine the lists into one
+    listOfEventLists = pageOneListOfEventLists + pageTwoListOfEventLists
 
-        # get a list of event dicts
-        listOfEventDicts = EventListParsing.listOfEventListsToListOfEventDicts(listOfEventLists)
+    # get a list of event dicts
+    listOfEventDicts = EventListParsing.listOfEventListsToListOfEventDicts(listOfEventLists)
 
-        # add event hours to the event dicts
-        listOfEventDicts = EventListParsing.addEventHours(listOfEventDicts)
+    # add event hours to the event dicts
+    listOfEventDicts = EventListParsing.addEventHours(listOfEventDicts)
 
-        # create a dict of lists, one for each hour where there's an event
-        visualiserDict = EventListParsing.createWishlistVisualiserDict(listOfEventDicts)
+    # create a dict of lists, one for each hour where there's an event
+    visualiserDict = EventListParsing.createWishlistVisualiserDict(listOfEventDicts)
 
-        # create an HTML table from the visualiser dict
-        visualiserHTML = EventListParsing.createHTMLVisualisation(visualiserDict)
+    # create an HTML table from the visualiser dict
+    visualiserHTML = EventListParsing.createHTMLVisualisation(visualiserDict)
 
-        # display the table
-        st.html(visualiserHTML)
+    # display the table
+    st.html(visualiserHTML)
